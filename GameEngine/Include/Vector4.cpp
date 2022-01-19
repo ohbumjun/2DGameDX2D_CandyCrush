ee03f09f99b1c322d2203a15908ddcb3e5ecd258
ohbumjun <beomjun0638@hanmail.net>
2022-01-19 15:03:22 +0900

    [Feat] ( Engine ) Create Vector4 Class

--- GameEngine/Include/Vector4.cpp
+++ GameEngine/Include/Vector4.cpp
@@ -1,1 +1,147 @@
 #include "Vector4.h"
+
+Vector4::Vector4() :
+x(0.f),
+y(0.f),
+z(0.f),
+w(0.f)
+{}
+
+Vector4::Vector4(float _x, float _y, float _z, float _w) :
+x(_x),
+y(_y),
+z(_z),
+w(_w)
+{}
+
+Vector4::Vector4(const Vector4& v) :
+x(v.x),
+y(v.y),
+z(v.z),
+w(v.w)
+{}
+
+Vector4::Vector4(const XMVECTOR& v)
+{
+	XMStoreFloat4((XMFLOAT4*)this, v);
+}
+
+Vector4& Vector4::operator=(const Vector4& v)
+{
+	x  = v.x;
+	y  = v.y;
+	z  = v.z;
+	w = v.w;
+
+	return *this;
+}
+
+Vector4& Vector4::operator=(const XMVECTOR& v)
+{
+	XMStoreFloat4((XMFLOAT4*)this, v);
+	return *this;
+}
+
+bool Vector4::operator==(const Vector4& v) const 
+{
+	return x == v.x && y == v.y && z == v.z && w == v.w;
+}
+
+bool Vector4::operator==(const XMVECTOR& v) const 
+{
+	Vector4 v1;
+	XMStoreFloat4((XMFLOAT4*)&v1, v);
+	return x == v1.x && y == v1.y && z == v1.z && w == v1.w;
+}
+
+bool Vector4::operator!=(const Vector4& v) const 
+{
+	return x != v.x && y != v.y && z != v.z && w != v.w;
+}
+
+bool Vector4::operator!=(const XMVECTOR& v) const 
+{
+	Vector4 v1;
+	XMStoreFloat4((XMFLOAT4*)&v1, v);
+	return x != v1.x && y != v1.y && z != v1.z && w != v1.w;
+}
+
+Vector4 Vector4::operator+(const Vector4& v) const 
+{
+	return Vector4(Convert() + v.Convert());
+}
+
+Vector4 Vector4::operator+(const XMVECTOR& v) const 
+{}
+
+Vector4 Vector4::operator+(float v) const 
+{}
+
+void Vector4::operator+=(const Vector4& v)
+{}
+
+void Vector4::operator+=(const XMVECTOR& v)
+{}
+
+void Vector4::operator+=(float v)
+{}
+
+Vector4 Vector4::operator-(const Vector4& v) const 
+{}
+
+Vector4 Vector4::operator-(const XMVECTOR& v) const 
+{}
+
+Vector4 Vector4::operator-(float v) const 
+{}
+
+void Vector4::operator-=(const Vector4& v)
+{}
+
+void Vector4::operator-=(const XMVECTOR& v)
+{}
+
+void Vector4::operator-=(float v)
+{}
+
+Vector4 Vector4::operator*(const Vector4& v) const 
+{}
+
+Vector4 Vector4::operator*(const XMVECTOR& v) const 
+{}
+
+Vector4 Vector4::operator*(float v) const 
+{}
+
+void Vector4::operator*=(const Vector4& v)
+{}
+
+void Vector4::operator*=(const XMVECTOR& v)
+{}
+
+void Vector4::operator*=(float v)
+{}
+
+Vector4 Vector4::operator/(const Vector4& v) const 
+{}
+
+Vector4 Vector4::operator/(const XMVECTOR& v) const 
+{}
+
+Vector4 Vector4::operator/(float v) const 
+{}
+
+void Vector4::operator/=(const Vector4& v)
+{}
+
+void Vector4::operator/=(const XMVECTOR& v)
+{}
+
+void Vector4::operator/=(float v)
+{}
+
+void Vector4::Convert(const XMVECTOR& v)
+{}
+
+XMVECTOR Vector4::Convert()
+{}
--- GameEngine/Include/Vector4.h
+++ GameEngine/Include/Vector4.h
@@ -1,5 +1,60 @@
 #pragma once
+
+#include "EngineMath.h"
+
 class Vector4
 {
+	float x, y, z, w;
+public :
+	Vector4();
+	Vector4(float _x, float _y, float _z, float _w);
+	Vector4(const Vector4& v);
+	Vector4(const XMVECTOR& v);
+
+	// 대입
+	Vector4& operator = (const Vector4& v);
+	Vector4& operator = (const XMVECTOR& v);
+
+	// ==, !=
+	bool operator == (const Vector4& v) const;
+	bool operator == (const XMVECTOR& v) const;
+	bool operator != (const Vector4& v) const;
+	bool operator != (const XMVECTOR& v) const;
+
+	// 연산자 오버로딩
+	Vector4 operator + (const Vector4& v) const;
+	Vector4 operator + (const XMVECTOR& v) const;
+	Vector4 operator + (float v) const;
+	void operator += (const Vector4& v);
+	void operator += (const XMVECTOR& v);
+	void operator += (float v);
+
+	Vector4 operator - (const Vector4& v) const;
+	Vector4 operator - (const XMVECTOR& v) const;
+	Vector4 operator - (float v) const;
+	void operator -= (const Vector4& v);
+	void operator -= (const XMVECTOR& v);
+	void operator -= (float v);
+
+	Vector4 operator * (const Vector4& v) const;
+	Vector4 operator * (const XMVECTOR& v) const;
+	Vector4 operator * (float v) const;
+	void operator *= (const Vector4& v);
+	void operator *= (const XMVECTOR& v);
+	void operator *= (float v);
+
+	Vector4 operator / (const Vector4& v) const;
+	Vector4 operator / (const XMVECTOR& v) const;
+	Vector4 operator / (float v) const;
+	void operator /= (const Vector4& v);
+	void operator /= (const XMVECTOR& v);
+	void operator /= (float v);
+
+	// 길이, Normalize, 거리
+
+	// Convert
+	void Convert(const XMVECTOR& v);
+	XMVECTOR Convert();
+
 };
 
