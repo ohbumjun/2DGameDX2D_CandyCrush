a99e11a0f9d4a7bf366e1fea77a6a0eb5bb23eea
ohbumjun <beomjun0638@hanmail.net>
2022-01-21 14:14:54 +0900

    [Feat] ( Engine ) Createe CEngine Class

--- GameEngine/Include/SharedPtr.h
+++ GameEngine/Include/SharedPtr.h
@@ -1,1 +1,68 @@
 #pragma once
+
+template<typename T>
+class CSharedPtr {
+public :
+	CSharedPtr() : m_Ptr(nullptr){}
+	CSharedPtr(T* m_Ptr)
+{
+		m_Ptr = m_Ptr;
+		m_Ptr->AddRef();
+}
+	CSharedPtr(const CSharedPtr<T>& Ptr)
+{
+		m_Ptr = Ptr.m_Ptr;
+		m_Ptr->AddRef();
+}
+	~CSharedPtr()
+{
+	if (m_Ptr)
+		m_Ptr->Release();
+}
+
+private :
+	T* m_Ptr;
+public :
+	void operator = (T* Ptr)
+{
+		if (m_Ptr)
+			m_Ptr->Release();
+		m_Ptr = Ptr;
+		m_Ptr->AddRef();
+}
+	void operator = (const CSharedPtr<T>& Ptr)
+	{
+		if (m_Ptr)
+			m_Ptr->Release();
+		m_Ptr = Ptr.m_Ptr;
+		m_Ptr->AddRef();
+	}
+	bool operator == (T* Ptr) const 
+{
+		return m_Ptr == Ptr;
+}
+	bool operator == (const CSharedPtr<T>& Ptr) const 
+{
+		return m_Ptr == Ptr.m_Ptr;
+}
+	bool operator != (T* Ptr) const
+{
+		return m_Ptr != Ptr;
+}
+	bool operator != (const CSharedPtr<T>& Ptr) const
+{
+		return m_Ptr != Ptr.m_Ptr;
+}
+	operator T* () const
+	{
+		return m_Ptr;
+	}
+	T* operator -> () const
+{
+		return m_Ptr;
+}
+	T* operator * () const
+{
+		return m_Ptr;
+}
+};
