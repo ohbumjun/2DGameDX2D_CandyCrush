731c4aaf5e75fa1ce24f0fbeb6fb764af3e2274e
ohbumjun <beomjun0638@hanmail.net>
2022-02-03 14:44:56 +0900

    [Feat] ( Engine ) Create Mesh2dShader Class

---
+++ GameEngine/Include/Resource/Shader/Mesh2DShader.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GraphicShader.h"
+class CMesh2DShader :
+    public CGraphicShader
+{
+    friend class CShaderManager;
+protected:
+    CMesh2DShader();
+    virtual ~CMesh2DShader();
+public :
+    virtual bool Init() override;
+};
+
+
