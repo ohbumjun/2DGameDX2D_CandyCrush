a3691def82db9a7e09a927e63d61b0ad2d482763
ohbumjun <beomjun0638@hanmail.net>
2022-04-15 19:39:13 +0900

    [Feat] (Client) Render ProgressBar To Screen

--- GameEngine/Include/Resource/Shader/NumberShader.cpp
+++ GameEngine/Include/Resource/Shader/NumberShader.cpp
@@ -17,7 +17,7 @@ bool CNumberShader::Init()
 	if (!LoadPixelShader("WidgetPS", TEXT("Widget.fx"), SHADER_PATH))
 		return false;
 
-	AddInputDesc("POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,
+	AddInputDesc("POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, //
 		0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0);
 	AddInputDesc("TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,
 		0, 8, D3D11_INPUT_PER_VERTEX_DATA, 0);
